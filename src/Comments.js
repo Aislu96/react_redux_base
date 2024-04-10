2c1940f31f7276c0a3c66dba5705aad0076cab55
Aislu Kharisova <arslanova1996ais@list.ru>
2024-04-10 12:20:54 +0300

    3й Компонент - Comments (используем Хуки)

---
+++ src/Comments.js
@@ -0,0 +1,29 @@
+import SingleComment from "./redux/SingleComment";
+import {useState} from "react";
+
+function Comments(props) {
+    console.log('comments props >', props);
+    const [textComment, setTextComment] = useState('');
+
+    const handleInput = (e) => {
+        console.log('input >>>', e.target.value);
+        setTextComment(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('submit textComment', textComment)
+    }
+
+    return (
+        <div className="card-comments">
+            <form onSubmit={handleSubmit} className='comments-item-create'>
+                <input type='text' value={textComment} onChange={handleInput}/>
+                <input type='submit' hidden/>
+            </form>
+            <SingleComment/>
+        </div>
+    )
+}
+
+export default Comments;
--- src/Title.js
+++ src/Title.js
@@ -3,11 +3,12 @@ import {inputText} from "./redux/action";
 
 function Title(props) {
     console.log("props title >", props);
+    // Получить данные из состояния(состояния) хранилища(store) Redux с помощью функции селектора.
     const text = useSelector(state => {
         const {inputReducer} = state;
         return inputReducer.text;
     });
-
+//Этот хук возвращает ссылку на функцию dispatch из Redux хранилища(store).
     const dispatch = useDispatch();
 
     const handleChange = (e) => {
