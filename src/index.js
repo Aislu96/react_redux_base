5912238d654f4f6111234ef9ac7e9425086199c1
Aislu Kharisova <arslanova1996ais@list.ru>
2024-04-12 12:52:51 +0300

    Добавление middleware.

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ import App from './App';
 import {Provider} from "react-redux";
 import {createStore, compose, applyMiddleware} from 'redux';//Создает хранилище Redux , в котором хранится полное дерево состояний вашего приложения.
 import {rootReducer} from "./redux/rootReducer";
+import {spamFilter} from "./redux/middleware";
 
 const store = createStore(rootReducer, compose(
     applyMiddleware(
-        thunk
+        thunk,
+        spamFilter
     ),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ));
